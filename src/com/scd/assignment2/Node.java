4557752f486c107852a5d3df75ac2fee6a77e0a6
Abdul Ahad <Abdul Ahad@DESKTOP-M5AG1H7>
2023-10-03 12:09:19 +0500

    First Commit

---
+++ src/com/scd/assignment2/Node.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.scd.assignment2;
+
+/**
+ *
+ * @author Abdul Ahad
+ */
+public class Node {
+    char data;
+    Node next;
+    Node prev;
+    Node(){
+        data = '\0';
+        next = null;
+        prev = null;
+    }
+}
+
