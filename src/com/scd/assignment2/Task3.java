59fac56af93fde19a25b4133f9be6b402e192a76
Abdul Ahad <Abdul Ahad@DESKTOP-M5AG1H7>
2023-10-03 21:57:27 +0500

    Seventh Commit

--- src/com/scd/assignment2/Task3.java
+++ src/com/scd/assignment2/Task3.java
@@ -21,22 +21,24 @@ public class Task3 {
         
         int[] interval2 = new int[2];
         interval2[0] = 2;
-        interval2[1] = 6;
+        interval2[1] = 4;
         intervals.add(interval2);
 
         int[] interval3 = new int[2];
         interval3[0] = 2;
-        interval3[1] = 4;
+        interval3[1] = 6;
         intervals.add(interval3);
         
         int[] interval4 = new int[2];
         interval4[0] = 15;
         interval4[1] = 18;
         intervals.add(interval4);
+        System.out.println("Given Intervals :-");
         for (int i = 0; i < intervals.size();i++)
             System.out.println("[ " + intervals.get(i)[0] + ", " + intervals.get(i)[1] + " ]");
         
         mergeIntervals(intervals);
+        System.out.println("After Merging Intervals :-");
         for (int i = 0; i < intervals.size();i++)
             System.out.println("[ " + intervals.get(i)[0] + ", " + intervals.get(i)[1] + " ]");
 
@@ -46,16 +48,27 @@ public class Task3 {
         
         for (int i = 0; i < intervals.size() - 1;){
             
-            if ((intervals.get(i)[1] > intervals.get(i + 1)[0]) && (intervals.get(i)[1] < intervals.get(i + 1)[1])){
+            if ((intervals.get(i)[1] >= intervals.get(i + 1)[0]) && (intervals.get(i)[1] <= intervals.get(i + 1)[1])){
                 intervals.get(i)[1] = intervals.get(i + 1)[1];
                 intervals.remove(i + 1);
             }
-            else if ((intervals.get(i)[1] > intervals.get(i + 1)[0]) && (intervals.get(i)[1] > intervals.get(i + 1)[1])){
+            else if ((intervals.get(i)[1] >= intervals.get(i + 1)[0]) && (intervals.get(i)[1] >= intervals.get(i + 1)[1])){
                 intervals.remove(i + 1);
             }
             else{
                 i++;
             }
+        }
+        
+        int lastIndex = intervals.size() - 1;
+        int secondlastIndex = intervals.size() - 2;
+        
+        if ((intervals.get(secondlastIndex)[1] >= intervals.get(lastIndex)[0]) && (intervals.get(secondlastIndex)[1] <= intervals.get(lastIndex)[1])){
+            intervals.get(secondlastIndex)[1] = intervals.get(lastIndex)[1];
+            intervals.remove(lastIndex);
+        }
+        else if ((intervals.get(secondlastIndex)[1] >= intervals.get(lastIndex)[0]) && (intervals.get(secondlastIndex)[1] >= intervals.get(lastIndex)[1])){
+            intervals.remove(lastIndex);
         }
     }
 }
